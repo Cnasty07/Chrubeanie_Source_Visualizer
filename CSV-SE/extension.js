37107cc9382f97b96d2fdcd868bcbfea9b3a0d82
CAN07 <115360508+Cnasty07@users.noreply.github.com>
2024-10-21 07:39:26 -0500

    changed html and added active editor change

--- CSV-SE/extension.js
+++ CSV-SE/extension.js
@@ -27,7 +27,7 @@ function activate(context) {
 			const panel = vscode.window.createWebviewPanel(
 				'SCActive',
 				'Source Control Visualizer',
-				vscode.ViewColumn.One,
+				vscode.ViewColumn.Two,
 				{
 					localResourceRoots: [vscode.Uri.joinPath(context.extensionUri,"CSV-SE/viewHTML.html")]
 				}
@@ -45,11 +45,20 @@ function activate(context) {
 			*/
 			// INFO: Picks the open document from the vscode window to use when command is started
 			let active_document = vscode.window.activeTextEditor
-			vscode.window.showTextDocument(active_document.document)
+			// vscode.window.showTextDocument(active_document.document)
+			console.log(active_document.document.languageId)
 			let new_doc = vscode.Uri.parse(active_document.document.fileName)
+			panel.webview.html = getWebViewSourceVisual(active_document.document)
+
 			
 			// function for updating webview, currently set to every 5 seconds 
 			const updateWebView = async () => {
+				// active editor change
+				vscode.window.onDidChangeActiveTextEditor((response) => {
+					active_document = response;
+					console.log(active_document.document.fileName)
+					console.log()
+				})
 				let DSP = await vscode.commands.executeCommand("vscode.executeDocumentSymbolProvider", new_doc).then((data) => { return data })
 				let new_doc_symbols = Object.assign({},DSP)
 				console.log(await new_doc_symbols[0])
@@ -59,12 +68,11 @@ function activate(context) {
 						console.log(element)
 					}
 				}
-				panel.webview.html = getWebViewSourceVisual(new_doc_symbols[0].name)
+				// panel.webview.html = getWebViewSourceVisual(new_doc_symbols[0].name)
 				console.log(`${new_doc_symbols[4]} updated`)
 			}
 			updateWebView()
 
-			
 			const intr = setInterval(updateWebView,5000)
 
 			// when closing webview clears interval
@@ -74,10 +82,11 @@ function activate(context) {
 			null,
 			context.subscriptions
 			);
-			
+
 		})
-	
+		
 	);
+	// context.subscriptions.push( vscode.commands.registerCommand())
 	
 }
 // idk
@@ -90,19 +99,40 @@ async function get_html() {
 }
 
 // temporary web page render until custom panel is complete
+// FIXME: need to add dynamic data
 function getWebViewSourceVisual(activeEditor) {
 	return `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Source</title>
+    <title>Source Visualizer</title>
 </head>
 <body>
-	<h1>Source Visualizer</h1>
+	<h1>Source Visualizer -Chrubeanie </h1>
+	<h3>File: ${activeEditor.fileName} </h3>
+	<h4>Lang -> ${activeEditor.languageId}</h4>
 	<br>
-	${get_html()}
-	
+	     <thead>
+				<tr>
+					<th scope="col">Column 1</th>
+					<th scope="col">Column 2</th>
+					<th scope="col">Column 3</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr class="">
+					<td scope="row">R1C1</td>
+					<td>R1C2</td>
+					<td>R1C3</td>
+				</tr>
+				<tr class="">
+					<td scope="row">Item</td>
+					<td>Item</td>
+					<td>Item</td>
+				</tr>
+			</tbody>
+
 </body>
 </html>`
 }
